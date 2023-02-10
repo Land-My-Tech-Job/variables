4109f3890dbbd4c77bd63f85b299b97649b4b5b1
Demond Harris <Dae@demonds-mbp.myfiosgateway.com>
2023-02-10 12:19:21 -0500

    updates

---
+++ aprilVariables.js
@@ -0,0 +1,42 @@
+// # Problem One
+
+//const birthYear = 1989
+
+//let futureYear = 2026
+
+//let age = futureYear - birthYear
+
+//console.log('I will be '+ age  + ' or ' + (age - 1) + ' in ' + futureYear)
+
+// # Problem Two
+//let currentAge = 34
+//let maxAge = 40
+//let snackAmount = 4 
+
+ //let lifetimeSnacks = (maxAge - currentAge) * snackAmount
+//console.log('You will need ' + lifetimeSnacks + ' to last you until the age of ' + maxAge)
+
+//Problem 3
+//let radius = 10
+//let circumferenceResult = (3.14 * radius) * 2
+//console.log("The circumference is" + circumferenceResult)
+
+//Problem Four
+//tempInCelsius = 100
+//let tempInFahrenheit = (tempInCelsius * 1.8)
+//console.log(tempInCelsius + '°C is = ' + tempInFahrenheit + '°F')
+
+//Problem Five
+//let aliceGrade = 84
+//let bobGrade = 74
+//let camGrade = 95
+//let averageGrade = (aliceGrade + bobGrade + camGrade) / 3
+//console.log(averageGrade)
+//let deeGrade = 62
+//let sumGrade = aliceGrade + bobGrade + camGrade + deeGrade
+//let studentAverage = sumGrade / 4
+//console.log(studentAverage)
+//console.log(deeGrade <= studentAverage)
+
+
+
